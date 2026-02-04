5a171b9130f0f94fe4866ccc332c87bbbf68d145
Alexandre COSTE <coste.alexandre09@gmail.com>
2026-02-04 18:40:36 +0100

    Add history feature and correction of bugs

--- calculator.py
+++ calculator.py
@@ -7,12 +7,13 @@ from operations import operation
 print("Give me two numbers, and I'll applicate operations on them")
 print("Enter 'q' at anytime to quit.")
 
+history = []
 while True:
     number_one = input("\nGive me a number: ")
 
     #Test if the user wants to quit
     if number_one.lower().strip() == 'q':
-        break
+        exit()
 
     #Test if the input is valid
     try:
@@ -54,14 +55,23 @@ while True:
     #Operation
     try:
         calculation = operation(number_one, number_two, operator)
-        print(f"\n{number_one:.2f} / {number_two:.2f} = {calculation:.2f}")
+        print(f"\n{number_one:.2f} {operator} {number_two:.2f} = {calculation:.2f}")
+        history.append(f"{number_one:.2f} {operator} {number_two:.2f} = {calculation:.2f}")
     except ZeroDivisionError:
         print("Oops! Division by 0 is not valid...")
     
     #Test if the user wants to make another calculation
     more_calculation = input("\nDo you want make another operation? (y/n) \n")
-    if more_calculation != 'y':
+    if more_calculation.lower().strip() != 'y' and more_calculation.lower().strip() != 'yes':
         break
+
+#History display
+if history:
+    history_list = input("Do you want your history calculation? (y/n) ")
+    if history_list.lower().strip() in ["y", "yes"]:
+        for calculation in history:
+            print(calculation)
+
 
 
     
